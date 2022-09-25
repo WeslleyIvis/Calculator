ab00722f5d68e6c84fd908592fac70ba7aacdec7
WeslleyIvis <weslley_reive@hotmail.com>
2022-09-25 19:05:54 -0300

    Tabela automatica, atv faculdade

--- ex/atividadesJS/script.js
+++ ex/atividadesJS/script.js
@@ -1,85 +1,98 @@
-// 6) Uma empresa irá dar 25% de aumento para todos os funcionários, você deverá desenvolver um programa que leia o salário atual do funcionário. Após a leitura, calcule e mostre na própria página o salário atual em uma linha e em outra linha o salário com o aumento de 25%.
+// 1) Criar um script que solicite ao usuário o valor total da compra e a quantidade de parcelas a financiar e o sistema deve visualizar o valor de cada parcela de acordo com os juros da tabela abaixo. Utilize campos de texto para ler os valores e dar a resposta ao usuário:
 
-const valorSalario = document.querySelector('#salario');
-const printValor = document.querySelector('.valor');
-const valorPorc = document.querySelector('.valor-porce');
+const valueParcelas = document.querySelectorAll('.input-p');
+const tableParcelas = document.querySelector('.table-parcelas');
 
-valorSalario.addEventListener('keyup', () => {
-  printValor.innerHTML = `Valor Atual: R$ ${parseFloat(valorSalario.value)}`;
-  valorPorc.innerHTML = `Valor com aumento: R$ ${
-    (parseFloat(valorSalario.value) * 25) / 100 + parseFloat(valorSalario.value)
-  }`;
+valueParcelas.forEach((element) => {
+	element.addEventListener('keyup', () => {
+		let html = `        
+			<tr class="thead-color">
+				<td>N° Parcelas</td>
+				<td>Valor da Parcela</td>
+				<td>% de Juros</td>
+				<td>Juros Parcela</td>
+				<td>Juros Total</td>
+			</tr>`;
+		let parcelas = 0;
+		let valorP = parseFloat(valueParcelas[0].value);
+		let juros = 0;
+		let valorJurosM = 0;
+		let valorJurosT= 0;
+		
+		if (valueParcelas[1].value < 2) {
+			juros = 0;
+		} else if (valueParcelas[1].value >= 2 && valueParcelas[1].value < 5) {
+			juros = 2;
+		} else {
+			juros = (70 / 100).toFixed(3);
+		}
+
+		for (let i = 0; i < +valueParcelas[1].value; i++) {
+			html += `
+			<tr>
+			<td>${parcelas += 1}</td>
+			<td>${valorP.toFixed(2)} </td>
+			<td>% ${juros} </td>
+			<td>${valorJurosM.toFixed(2)} </td>
+			<td>${valorJurosT.toFixed(2)} </td>
+			</tr>`	
+
+			valorJurosM = valorP * juros * 1 / 100;
+			valorJurosT += valorJurosM
+			valorP = valorP + valorJurosM;
+		}
+		tableParcelas.innerHTML = html;
+	})
 });
 
-//7) Crie um script que calcule e mostre na própria página a média aritmética de 4 números reais digitados pelo usuário.
+// 3) Faça um script que receba dois números inteiros e mostre qual é o maior ou se são iguais. Leia os valores por meio de um formulário e mostre a mensagem final em um campo de texto.
 
-const valorM = document.querySelectorAll('.valorMedia');
-const textMedia = document.querySelector('.textMedia');
+const valores = document.querySelectorAll(".valor-ex2");
+const textEx2 = document.querySelector(".text-ex2");
 
-valorM.forEach((element) => {
-  element.addEventListener('keyup', () => {
-    if (
-      +valorM[0].value != '' && +valorM[1].value != '' && +valorM[2].value != '' && +valorM[3].value != ''
-    ) {
-      const total =  +valorM[0].value + +valorM[1].value + +valorM[2].value + +valorM[3].value;
-      textMedia.innerHTML = `Média: ${total /4}`;
-    }
-  });
-});
+valores.forEach((element) => {
+	element.addEventListener('keyup', () => {
+		if (parseInt(valores[0].value) > parseInt(valores[1].value)) {
+			textEx2.innerHTML = `Maior número: ${parseInt(valores[0].value)}`
+		} 
+		else if (parseInt(valores[0].value) == parseInt(valores[1].value)) {
+			textEx2.innerHTML = `Os valores são iguais: ${parseInt(valores[0].value)}`
+		}
+		else {
+			textEx2.innerHTML = `Maior número: ${parseInt(valores[1].value)}`
+		}
+	})
+})
 
-// 8) Crie um script que calcule as raízes de uma equação de 2º grau ax2 + bx + c.
+// 4) Crie um arquivo HTML que utilizando JavaScript leia a base e a altura de um retângulo (por meio de um formulário), calcule sua área e apresente os resultados em um campo de texto ou textarea.
 
-const valorD = document.querySelectorAll('.valorDelta');
-const textD = document.querySelector('.textD');
+const retangulo = document.querySelectorAll('.valor-ex3');
+const textEx3 = document.querySelector('.text-ex3');
 
-valorD.forEach((element) => {
-  element.addEventListener('keyup', () => {
-    if (
-      +valorD[0].value != '' || +valorD[0].value == 0 &&
-      +valorD[1].value != '' || +valorD[1].value == 0 &&
-      +valorD[2].value != '' || +valorD[2].value == 0
-    ) {
-      calcDelta(+valorD[0].value, +valorD[1].value, +valorD[2].value);
-    };
-  });
-});
-
-function calcDelta(a, b, c) {
-  const delta = b ** 2 - 4 * a * c;
-  if (delta <= 0) {
-    textD.innerHTML = `Não pode ser negativo <br> Valor de delta: ${delta}`;
-  } else {
-    let raizPositiva = (-b + Math.sqrt(delta)) / (2 * a);
-    let raizNegativa = (-b - Math.sqrt(delta)) / (2 * a);
-    textD.innerHTML = `Raiz positiva: ${raizPositiva} <br> Raiz negativa: ${raizNegativa}`;    
-  };
-};
-
-// 9) Faça um script que leia o nome de uma pessoa, o ano de nascimento dessa pessoa e o ano atual, calcule e mostre a idade dessa pessoa e seu nome em linhas separadas.
-
-const nome = document.querySelectorAll(".calc-idade");
-const textCalc = document.querySelector(".textCalc");
-const data = new Date();
-console.log(data.getFullYear());
-
-nome.forEach((element) => {
-    element.addEventListener('keyup', () => {
-      textCalc.innerHTML = `Nome: ${nome[0].value} <br> Idade: ${data.getFullYear() - +nome[1].value}`
-    })
+retangulo.forEach((element) => {
+	element.addEventListener('keyup', () => {
+		if (retangulo[0].value != '' && retangulo[1].value != '') {
+			textEx3.innerText = `Valor: ${+retangulo[0].value * +retangulo[1].value}`
+		} 
+	})
 })
 
 
-// 10) Faça um script que receba a altura de um degrau de uma escada e a altura que um pedreiro deseja alcançar utilizando essa escada, calcule e mostre quantos degraus ele deverá subir para atingir seu objetivo, os valores fornecidos devem ser em metros.
+// 5) Faça um script que receba três notas reais (três campos de texto), calcule e mostre a média do aluno e o conceito conforme regras abaixo
 
+const notas = document.querySelectorAll(".valor-ex4");
+const textEx4 = document.querySelector(".text-ex4");
 
-const alturaDegrau = document.querySelectorAll('.calc-dg');
-const textDg = document.querySelector('.textDg');
+notas.forEach((element) => {
+	element.addEventListener('keyup', () => {		
+		let media = +` ${(parseFloat(notas[0].value) + parseFloat(notas[1].value) + parseFloat(notas[2].value)) / 3}`
 
-
-alturaDegrau.forEach((element) => {
-  element.addEventListener('keyup', () => {
-    if (alturaDegrau[0] != '' && alturaDegrau[1] != '') {
-      textDg.innerHTML = `Quantidade de Degraus: ${+alturaDegrau[1].value / +alturaDegrau[0].value}`
-    }
-  })
+		if (media >= 6) {
+			textEx4.innerHTML = "Aprovado"	
+		} else if (media >= 3 && media < 6) {
+			textEx4.innerText = "Exame"
+		} else {
+			textEx4.innerText = "Reprovado"
+		}
+	})
 })
